785abc71060a7650f6ccaa2569de81e81a2c722f
Shenghao Mo <78923489+Akiramex@users.noreply.github.com>
2023-04-26 09:57:37 +0800

    Add files via upload

---
+++ part01/get_buf.c
@@ -0,0 +1,41 @@
+/* ************************************************************************
+> File Name:     code/TCP_IP_code/get_buf.c
+> Author:        Akira
+> Created Time:  Sun 09 Apr 2023 08:22:56 AM CST
+> Description:   
+ ************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+void error_handling(char *message);
+
+int main(int argc, char argv[])
+{
+    int sock;
+    int snd_buf, rcv_buf, state;
+    socklen_t len;
+    
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+
+    len = sizeof(snd_buf);
+    state = getsockopt(sock, SOL_SOCKET, SO_SNDBUF, (void*)&snd_buf, &len);
+    if(state)
+        error_handling("getsockopt() error");
+
+    len = sizeof(rcv_buf);
+    state = getsockopt(sock, SOL_SOCKET, SO_RCVBUF, (void*)&rcv_buf, &len);
+    if(state)
+        error_handling("getsockopt() error");
+
+    printf("Input buffer size: %d \n", rcv_buf);
+    printf("Output buffer size: %d \n", snd_buf);
+    return 0;
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
